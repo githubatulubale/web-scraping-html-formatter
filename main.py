4d854a2c58f0d5a54abda259524acc9570ecc9cc
githubatulubale <atulubale347@gmail.com>
2025-12-19 15:07:29 +0530

    updated funn main

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import re     # regular expression
 import bs4    # formatting   
 import os     # file path    
 
-url = input("Enter URL:")
-response = requests.get(url)
-# print(type(response))
-# print(response.text)
+def main():
+    url = input("Enter URL:")
+    response = requests.get(url)
+    # print(type(response))
+    # print(response.text)
 
-filename = "temp1.html"
-bs = bs4.BeautifulSoup(response.text, "html.parser")
-formated_text = bs.prettify()
-# print(formated_text)
-with open(filename, "w+") as f:
-    f.write(formated_text)
-    
+    filename = "temp1.html"
+    bs = bs4.BeautifulSoup(response.text, "html.parser")
+    formated_text = bs.prettify()
+    # print(formated_text)
+    with open(filename, "w+") as f:
+        f.write(formated_text)
+
+if __name__ == "__main__":
+    main()
